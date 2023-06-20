c42786025912df28b395648b474a4442af9fdb03
Grant Findlay <dev@grantfindlay.com>
2023-06-20 09:24:27 +0100

    update markdown header sizes

--- components/markdown/index.tsx
+++ components/markdown/index.tsx
@@ -1,11 +1,29 @@
 import ReactMarkdown from "react-markdown";
 import ChakraUIRenderer from "chakra-ui-markdown-renderer";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Heading } from "@chakra-ui/react";
 
 export const Markdown = ({ children }: { children: string }) => {
   return (
     <ReactMarkdown
       components={ChakraUIRenderer({
+        h1: (props) => (
+          <Heading as="h1" fontSize="2xl" mt="6" mb="4" {...props} />
+        ),
+        h2: (props) => (
+          <Heading as="h2" fontSize="xl" mt="6" mb="4" {...props} />
+        ),
+        h3: (props) => (
+          <Heading as="h3" fontSize="lg" mt="6" mb="4" {...props} />
+        ),
+        h4: (props) => (
+          <Heading as="h4" fontSize="md" mt="6" mb="4" {...props} />
+        ),
+        h5: (props) => (
+          <Heading as="h5" fontSize="sm" mt="6" mb="4" {...props} />
+        ),
+        h6: (props) => (
+          <Heading as="h6" fontSize="xs" mt="6" mb="4" {...props} />
+        ),
         blockquote: (props) => (
           <Box
             as="blockquote"
