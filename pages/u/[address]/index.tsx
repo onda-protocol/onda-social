2a3dfeac9c2787e1e6af0737159c91ac2a31c171
Grant Findlay <dev@grantfindlay.com>
2023-09-13 16:58:07 +0100

    WIP adding profile modal

--- pages/u/[address]/index.tsx
+++ pages/u/[address]/index.tsx
@@ -15,7 +15,7 @@ import {
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import toast from "react-hot-toast";
 
 import { shortenAddress } from "utils/format";
@@ -23,10 +23,12 @@ import { fetchUser, fetchUserComments, fetchUserPosts } from "lib/api";
 import { CommentListItem } from "components/comment";
 import { useAuth } from "components/providers/auth";
 import { PostList } from "components/post/list";
+import { ProfileModal } from "components/modal/profile";
 
 const User: NextPage = () => {
   const router = useRouter();
   const auth = useAuth();
+  const [modal, setModal] = useState(false);
   const address = router.query.address as string;
   const query = useQuery(["user", address], () => fetchUser(address));
 
@@ -47,65 +49,68 @@ const User: NextPage = () => {
   }
 
   return (
-    <Container maxW="container.md">
-      <Box display="flex" flexDirection="column" alignItems="center" my="12">
-        <Avatar
-          size="xl"
-          name={query.data?.name ?? undefined}
-          src={query.data?.avatar ?? undefined}
-        />
-        <Box mt="6">
-          {query.data?.name ? (
-            <>
-              <Heading size="md" textAlign="center">
-                {query.data.name}
-              </Heading>
+    <>
+      <Container maxW="container.md">
+        <Box display="flex" flexDirection="column" alignItems="center" my="12">
+          <Avatar
+            size="xl"
+            name={query.data?.name ?? undefined}
+            src={query.data?.avatar ?? undefined}
+          />
+          <Box mt="6">
+            {query.data?.name ? (
+              <>
+                <Heading size="md" textAlign="center">
+                  {query.data.name}
+                </Heading>
+                <Box
+                  aria-label="Copy address to clipboard"
+                  fontSize="xs"
+                  textAlign="center"
+                  color="gray.500"
+                  onClick={handleCopyAddress}
+                >
+                  {authorAddress}
+                </Box>
+              </>
+            ) : (
               <Box
                 aria-label="Copy address to clipboard"
-                fontSize="xs"
+                fontSize="xl"
+                fontWeight="bold"
                 textAlign="center"
-                color="gray.500"
                 onClick={handleCopyAddress}
               >
                 {authorAddress}
               </Box>
-            </>
-          ) : (
-            <Box
-              aria-label="Copy address to clipboard"
-              fontSize="xl"
-              fontWeight="bold"
-              textAlign="center"
-              onClick={handleCopyAddress}
-            >
-              {authorAddress}
+            )}
+          </Box>
+          {isCurrentUser && (
+            <Box mt="6">
+              <Button size="sm" onClick={() => setModal(true)}>
+                Edit Profile
+              </Button>
             </Box>
           )}
         </Box>
-        {isCurrentUser && (
-          <Box mt="6">
-            <Link href={`/u/${address}/edit`}>
-              <Button size="sm">Edit Profile</Button>
-            </Link>
-          </Box>
-        )}
-      </Box>
-      <Tabs>
-        <TabList>
-          <Tab>Posts</Tab>
-          {/* <Tab>Comments</Tab> */}
-        </TabList>
+        <Tabs>
+          <TabList>
+            <Tab>Posts</Tab>
+            {/* <Tab>Comments</Tab> */}
+          </TabList>
 
-        <TabPanels>
-          <TabPanel px="0">
-            <PostsTab id={address} />
-          </TabPanel>
-          {/* <TabPanel>
+          <TabPanels>
+            <TabPanel px="0">
+              <PostsTab id={address} />
+            </TabPanel>
+            {/* <TabPanel>
             <CommentsTab id={address} />
           </TabPanel> */}
-        </TabPanels>
-      </Tabs>
-    </Container>
+          </TabPanels>
+        </Tabs>
+      </Container>
+      <ProfileModal open={modal} onRequestClose={() => setModal(false)} />
+    </>
   );
 };
 
