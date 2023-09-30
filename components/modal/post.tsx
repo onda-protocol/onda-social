c2dabdcafc579da55c05483a8871770bfec98b62
Grant Findlay <dev@grantfindlay.com>
2023-09-30 12:53:36 +0100

    post modal updates

--- components/modal/post.tsx
+++ components/modal/post.tsx
@@ -1,14 +1,32 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
-import { Box, Container, Portal, Fade, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Box,
+  Button,
+  Container,
+  Fade,
+  Flex,
+  Heading,
+  Portal,
+} from "@chakra-ui/react";
+import { useLayoutEffect, useMemo } from "react";
+import { IoClose, IoDocumentText, IoLink } from "react-icons/io5";
 
+import { SerializedPost } from "lib/api";
 import { Comments } from "components/comment/comments";
-import { IoArrowBack } from "react-icons/io5";
-import { useLayoutEffect } from "react";
+import { PostType } from "@prisma/client";
 
 export const PostModal = () => {
   const router = useRouter();
   const postId = router.query?.postId as string | undefined;
+
+  const postQueryKey = useMemo(() => ["post", postId], [postId]);
+  const postQuery = useQuery<unknown, unknown, SerializedPost>({
+    queryKey: postQueryKey,
+    refetchOnMount: false,
+  });
+  const post = postQuery.data;
 
   useLayoutEffect(() => {
     if (postId) {
@@ -18,6 +36,19 @@ export const PostModal = () => {
       document.body.style.overflow = "auto";
     };
   }, [postId]);
+
+  const icon = useMemo(() => {
+    switch (post?.postType) {
+      case PostType.TEXT:
+        return <IoDocumentText />;
+
+      case PostType.LINK:
+        return <IoLink />;
+
+      default:
+        return null;
+    }
+  }, [post]);
 
   return (
     <Portal>
@@ -42,19 +73,35 @@ export const PostModal = () => {
             maxW="container.lg"
             backgroundColor="onda.1050"
           >
-            <Button
-              aria-label="Back Button"
-              as={Link}
-              href="/"
-              scroll={false}
-              variant="ghost"
-              leftIcon={<IoArrowBack />}
-            >
-              Back
-            </Button>
+            <Flex height="42px" align="center" justify="space-between">
+              <Flex align="center">
+                <Box pr="2" color="gray.300">
+                  {icon}
+                </Box>
+                <Heading fontSize="md" fontWeight="medium" color="gray.300">
+                  {postQuery.data?.title}
+                </Heading>
+              </Flex>
+
+              <Button
+                as={Link}
+                href="/"
+                scroll={false}
+                variant="ghost"
+                fontSize="sm"
+                leftIcon={<IoClose size={18} />}
+              >
+                Close
+              </Button>
+            </Flex>
           </Container>
           <Box height="calc(100% - 42px)" width="100%" overflowY="scroll">
-            <Container maxW="container.lg" backgroundColor="onda.1000" pt="6">
+            <Container
+              maxW="container.lg"
+              height="100%"
+              backgroundColor="onda.1000"
+              pt="6"
+            >
               <Container
                 maxW="container.md"
                 onClick={(e) => {
