fecff33d5ec8b874b42c9d3d99dbe100ceb374d7
Grant Findlay <dev@grantfindlay.com>
2023-09-30 11:58:35 +0100

    some layout fixes

--- components/post/meta.tsx
+++ components/post/meta.tsx
@@ -24,52 +24,10 @@ interface PostMetaProps {
 }
 
 const Dot = () => (
-  <Box as="span" fontSize="xx-small" color="gray.500">
+  <Box as="span" fontSize="xx-small" color="gray.500" mx="0.5">
     &nbsp;•&nbsp;
   </Box>
 );
-
-const awardsArray = [
-  {
-    id: "0",
-    image: "/glass.png",
-    name: "Chewed Glass",
-    description:
-      "Activated charcoal affogato truffaut pour-over tumblr pop-up taiyaki.",
-  },
-  {
-    id: "1",
-    amount: 10_000_000,
-    image: "/bottle.png",
-    name: "Message in a Bottle",
-    description:
-      "Activated charcoal affogato truffaut pour-over tumblr pop-up taiyaki.",
-  },
-  {
-    id: "2",
-    amount: 10_000_000,
-    image: "/plankton.png",
-    name: "Plankton",
-    description:
-      "Activated charcoal affogato truffaut pour-over tumblr pop-up taiyaki.",
-  },
-  {
-    id: "3",
-    amount: 20_000_000,
-    image: "/crab.png",
-    name: "Crab",
-    description:
-      "Activated charcoal affogato truffaut pour-over tumblr pop-up taiyaki.",
-  },
-  {
-    id: "4",
-    amount: 30_000_000,
-    image: "/glasseater-dark.png",
-    name: "The Gigabrained Glass Eater",
-    description:
-      "Activated charcoal affogato truffaut pour-over tumblr pop-up taiyaki.",
-  },
-];
 
 export const PostMeta: React.FC<PostMetaProps> = ({
   isOp,
@@ -170,21 +128,25 @@ export const PostMeta: React.FC<PostMetaProps> = ({
       >
         <Box display="flex">
           {forum && (
-            <Text
+            <Box
               as="span"
-              fontSize="sm"
-              fontWeight="medium"
-              color="whiteAlpha.800"
-              _hover={{
-                color: "whiteAlpha.600",
-              }}
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="center"
+              whiteSpace="nowrap"
             >
               <Link href={`/o/${forumNamespace}`} onClick={handleClick}>
                 <Box
                   as="span"
                   display="flex"
                   alignItems="center"
-                  color="inherit"
+                  fontSize="sm"
+                  fontWeight="medium"
+                  color="whiteAlpha.800"
+                  _hover={{
+                    color: "whiteAlpha.600",
+                  }}
                 >
                   {displayIcon && forumIcon && (
                     <Box mr="2">
@@ -200,81 +162,73 @@ export const PostMeta: React.FC<PostMetaProps> = ({
                       />
                     </Box>
                   )}
-                  <Text as="span" color="inherit">
-                    <Text as="span" color="inherit" fontWeight="600">
-                      o/{forumNamespace ?? shortenAddress(forum)}
-                    </Text>
-                    <Dot />
+                  <Text as="span" color="inherit" fontWeight="600">
+                    o/{forumNamespace ?? shortenAddress(forum)}
                   </Text>
                 </Box>
               </Link>
-            </Text>
+              <Dot />
+            </Box>
           )}
-          <Text as="span" fontSize="sm">
-            <Box
-              as="span"
-              display="flex"
-              flexDirection="row"
-              alignItems="center"
-              justifyContent="center"
-              whiteSpace="nowrap"
-            >
-              <Link href={`/u/${author?.id}`} onClick={handleClick}>
-                <Box
-                  as="span"
-                  display="flex"
-                  alignItems="center"
-                  _hover={{
-                    textDecoration: "underline",
-                  }}
-                >
-                  {displayAvatar && author?.avatar && author?.name && (
-                    <Box as="span" mr="2">
-                      <Image
-                        height={24}
-                        width={24}
-                        alt={author.name}
-                        src={author.avatar}
-                        style={{
-                          borderRadius: "100%",
-                        }}
-                      />
-                    </Box>
-                  )}
-                  <Text
-                    as="span"
-                    color={forum ? "whiteAlpha.600" : "whiteAlpha.800"}
-                    fontWeight={forum ? "normal" : "medium"}
-                  >
-                    {forum ? "Posted by " : ""} {author?.name ?? authorAddress}
-                  </Text>
-                </Box>
-              </Link>
-              {isOp && (
+          <Box
+            as="span"
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="center"
+            whiteSpace="nowrap"
+            fontSize="sm"
+          >
+            <Link href={`/u/${author?.id}`} onClick={handleClick}>
+              <Box
+                as="span"
+                display="flex"
+                alignItems="center"
+                _hover={{
+                  textDecoration: "underline",
+                }}
+              >
+                {displayAvatar && author?.avatar && author?.name && (
+                  <Box as="span" mr="2">
+                    <Image
+                      height={24}
+                      width={24}
+                      alt={author.name}
+                      src={author.avatar}
+                      style={{
+                        borderRadius: "100%",
+                      }}
+                    />
+                  </Box>
+                )}
                 <Text
                   as="span"
-                  fontWeight="bold"
-                  fontSize="xs"
-                  color="steelBlue"
+                  color={forum ? "whiteAlpha.600" : "whiteAlpha.800"}
+                  fontWeight={forum ? "normal" : "medium"}
                 >
-                  &nbsp;OP
+                  {forum ? "Posted by " : ""} {author?.name ?? authorAddress}
                 </Text>
-              )}
-              <Dot />
-              <Text as="span" color="whiteAlpha.600">
-                {time}
+              </Box>
+            </Link>
+            {isOp && (
+              <Text as="span" fontWeight="bold" fontSize="xs" color="steelBlue">
+                &nbsp;OP
               </Text>
-              {lastEdited ? (
-                <>
-                  <Dot />
-                  <Text as="span" color="whiteAlpha.600">
-                    last edited&nbsp;
-                    {lastEdited}
-                  </Text>
-                </>
-              ) : null}
-            </Box>
-          </Text>
+            )}
+            <Dot />
+            <Text as="span" color="whiteAlpha.600">
+              {time}
+            </Text>
+            {lastEdited ? (
+              <>
+                <Dot />
+                <Text as="span" color="whiteAlpha.600">
+                  last edited&nbsp;
+                  {lastEdited}
+                </Text>
+              </>
+            ) : null}
+          </Box>
         </Box>
         {awardsEl && displayAward !== "large" && (
           <Box ml={{ sx: "-0.5", md: 0 }}>
